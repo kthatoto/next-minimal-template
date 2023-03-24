32c3c52ecabd8e1ca0ca765d60cdcfc14b853216
kthatoto <kthatoto@gmail.com>
2023-03-25 03:16:30 +0900

    Use style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import styles from "@/styles/Home.module.css";
+
 const Home = () => {
   return (
-    <h1>Next Minimal Template</h1>
+    <h1 className={styles.title}>Next Minimal Template</h1>
   );
 };
 export default Home;
